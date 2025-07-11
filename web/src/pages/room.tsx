69e96b9c29f44e997e63bd7fa1cfac1a72444a92
Denis <capelete.denis@hotmail.com>
2025-07-11 19:03:41 -0300

    feat(frontend): add useQuery to fetch and display rooms list

--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -5,7 +5,7 @@ type RoomParams = {
 }
 
 export function Room() {
-  const params = useParams()
+  const params = useParams<RoomParams>()
 
   if (!params.roomId) {
     return <Navigate replace to="/" />
