f1b06d1e7d6813bf83f5f406eb036566a8f05153
Denis <capelete.denis@hotmail.com>
2025-07-11 18:52:57 -0300

    fix(server): correct origin URL in fastifyCors to 'http://localhost:5173'

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ import { getRoomsRoute } from './http/routes/get-room.ts'
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: 'http:localhost:5173',
+  origin: 'http://localhost:5173',
 })
 
 app.setSerializerCompiler(serializerCompiler)
