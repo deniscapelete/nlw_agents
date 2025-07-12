64c3429c7b7eeef3edecafbbd992ced33e04020b
Denis <capelete.denis@hotmail.com>
2025-07-12 17:35:23 -0300

    feat(server): get questions

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,8 +7,9 @@ import {
 } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
-import { getRoomsRoute } from './http/routes/get-room.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -25,5 +26,6 @@ app.get('/health', () => {
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
+app.register(getRoomQuestions)
 
 app.listen({ port: env.PORT })
