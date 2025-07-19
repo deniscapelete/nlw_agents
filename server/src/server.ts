59258c0b84a2755df737f90e441225dcfad0fd0c
Denis <capelete.denis@hotmail.com>
2025-07-19 19:39:30 -0300

    feat(server): create audio_chunks table and endpoint to store audio chunks

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import { } from './db/connection.ts'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
@@ -11,12 +12,15 @@ import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { createQuestionRoute } from './http/routes/create-question.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -29,5 +33,6 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT, host: '127.0.0.1' })
